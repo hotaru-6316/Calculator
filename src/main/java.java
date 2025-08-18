fb8f5b68c9b75507cebe01f3125da867b868c862
hotaru-6316 <225122319+hotaru-6316@users.noreply.github.com>
2025-08-18 14:57:12 +0900

    テスト用の一時的なファイルの削除

--- src/main/java.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-import java.sql.*;
-import java.math.*;
-import java.util.Calendar;
-public class java {
-    public static void main(String... args) throws Exception {
-        Class.forName("org.h2.Driver");
-    }
-}
